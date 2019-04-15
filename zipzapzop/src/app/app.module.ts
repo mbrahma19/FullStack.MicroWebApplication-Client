4ec0aa7de623745b744ea23432e20ccce3ffb16c
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-04-15 16:05:52 -0400

    tags update

--- zipzapzop/src/app/app.module.ts
+++ zipzapzop/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { PostComponent } from './post/post.component';
-import { CommentComponent } from './comment/comment.component';
-import { TagComponent } from './tag/tag.component';
-import { FormsModule } from '@angular/forms';
-import {userService} from "./user/userService";
-import {User} from "./user/user";
-import {HttpModule} from "@angular/http";
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
-import {ReactiveFormsModule} from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { UserComponent } from "./user/user.component";
+import { PostComponent } from "./post/post.component";
+import { CommentComponent } from "./comment/comment.component";
+import { TagComponent } from "./tag/tag.component";
+import { FormsModule } from "@angular/forms";
+import { userService } from "./user/userService";
+import { User } from "./user/user";
+import { HttpModule } from "@angular/http";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
+import { ReactiveFormsModule } from "@angular/forms";
+import { TagService } from "./tag/tag.service";
+import { Tag } from "./tag/tag";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -21,25 +24,16 @@ import {ReactiveFormsModule} from "@angular/forms";
     PostComponent,
     CommentComponent,
     TagComponent
-
   ],
-
-
-
 
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     HttpModule,
-    HttpClientInMemoryWebApiModule,
-
-
-
+    HttpClientInMemoryWebApiModule
   ],
-  providers: [
-    userService,
-    User
-  ],
+  providers: [userService, User, TagService, Tag],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ zipzapzop/src/app/tag/tag.ts
@@ -0,0 +1,4 @@
+export class Tag {
+  id: number;
+  keyWord: string;
+}
