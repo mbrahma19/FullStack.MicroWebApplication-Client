7ed6d7c9d2111d9c5c5b7a16d8b03742f4c16338
shuchijain123 <sharma.shuchi1@gmail.com>
2019-04-17 09:53:11 -0400

    adding post componenet

--- zipzapzop/src/app/post/post.component.ts
+++ zipzapzop/src/app/post/post.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Post } from './post';
+
+import {postService}  from './postService';
+import {Router} from "@angular/router";
+import {User} from "../user/user";
+
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
+
+
+
 export class PostComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(public postService: postService) {
+
+
   }
 
+
+  post: Post = {
+
+    title1: 'Our Universal Blogs',
+    title: '',
+    body: '',
+    createDate: '',
+    tags: ''
+
+  };
+
+  private router: Router;
+  errorMessage: string;
+  posts: any[];
+  searchQuery='';
+
+
+  handleSuccess(res){
+
+
+    this.posts=res.json();
+
+    console.table(this.posts)
+  }
+
+
+getPosts(){
+
+
+
+return this.postService.getmyPosts(this.searchQuery)
+
+
+      .subscribe(data => {
+    this.handleSuccess(data);
+  });
+
+
+
+}
+
+
+
+
+  ngOnInit() {
+
+
+  }
 }
